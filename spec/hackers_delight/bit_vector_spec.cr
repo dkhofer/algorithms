bfd5d97c86b6d7c2f78724e704f1269800c5188f
David Hofer <david.hofer@gmail.com>
2016-02-19 13:24:48 -0500

    Add a pairwise match (for lack of a better name) function.

--- spec/hackers_delight/bit_vector_spec.cr
+++ spec/hackers_delight/bit_vector_spec.cr
@@ -13,12 +13,22 @@ describe "BitVector" do
   end
 
   it "computes the dot product for BigInt" do
-    v1 = BitVector.new(0xFFFFFFFFFFFFFFFF5555555555555555)
-    v2 = BitVector.new(0x5555555555555555FFFFFFFFFFFFFFFF)
+    v1 = BitVector.new(0xFFFFFFFFFFFFFFFF5555555555555555.to_big_i)
+    v2 = BitVector.new(0x5555555555555555FFFFFFFFFFFFFFFF.to_big_i)
     v1.dot_product(v2).should eq 0
 
-    v3 = BitVector.new(0x00000000000000000000000000000003)
+    v3 = BitVector.new(0x00000000000000000000000000000003.to_big_i)
     v1.dot_product(v3).should eq 1
     v2.dot_product(v3).should eq 0
+  end
+
+  it "finds if there is a pairwise match" do
+    v1 = BitVector.new(0xFFFFFFFFFFFFFFFF5555555555555555.to_big_i)
+    v2 = BitVector.new(0x5555555555555555FFFFFFFFFFFFFFFF.to_big_i)
+    v1.pairwise_match(v2).should eq 1
+
+    v3 = BitVector.new(0x00000000000000000000000000000002.to_big_i)
+    v1.pairwise_match(v3).should eq 0
+    v2.pairwise_match(v3).should eq 1
   end
 end
--- src/hackers_delight/bit_vector.cr
+++ src/hackers_delight/bit_vector.cr
@@ -1,12 +1,16 @@
 class BitVector
   property :elements
 
-  def initialize(bits)
-    @elements = bits
+  def initialize(bits = 0.to_big_i)
+    @elements = bits.to_big_i
   end
 
   def dot_product(other_vector)
     (@elements & other_vector.elements).popcount & 1
+  end
+
+  def pairwise_match(other_vector)
+    (@elements & other_vector.elements) > 0 ? 1 : 0
   end
 
   def ==(other : BitVector)
