91ce9c78d7255b4221858611d963b5bae8bb7b75
David Hofer <david.hofer@gmail.com>
2016-03-10 14:49:30 -0500

    Add comment about performance.

--- benchmark/hackers_delight/permutation.cr
+++ benchmark/hackers_delight/permutation.cr
@@ -2,8 +2,14 @@ require "benchmark"
 require "../../src/hackers_delight/permutation"
 
 puts "Creating perms..."
-normal_perms = (1..100).map { |i| Permutation.random(16_u32, false) }
-sag_perms = (0...100).map { |i| Permutation.new(normal_perms[i].point_map, true) }
+# NOTE(hofer): The more perms, the better the sag-style multiplication
+# works, I think because each new multiplication results in a single
+# 64-bit long instead of 16 32-bit ints (ie memory allocation costs a
+# decent amount).  NOT because of the sag algorithm itself, which is
+# significantly slower than the naive composition algorithm.  (eg, ~8x
+# speedup for 100K perms).
+normal_perms = (1..100_000).map { |i| Permutation.random(16_u32, false) }
+sag_perms = (0...100_000).map { |i| Permutation.new(normal_perms[i].point_map, true) }
 
 random_perm = Permutation.random(16_u32, true)
 normal_points = random_perm.point_map
