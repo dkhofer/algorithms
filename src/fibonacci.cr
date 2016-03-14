bd38d6ab12e57a82742c93fdefe7095894390bbb
David Hofer <david.hofer@gmail.com>
2016-03-13 21:36:22 -0400

    Use matrix shard instead of custom class.

--- src/fibonacci.cr
+++ src/fibonacci.cr
@@ -23,31 +23,6 @@ class Fibonacci
     zero = 0.to_big_i
     one = 1.to_big_i
     m = Matrix.rows([[one, one], [one, zero]])
-    (m ** n)[1, 1]
-  end
-end
-
-class FibMatrix
-  include Algebra
-
-  IDENTITY_VALUES = [[ONE, ONE], [ONE, ZERO]]
-
-  getter values
-
-  def initialize(vals = IDENTITY_VALUES)
-    @values = vals
-  end
-
-  def self.identity
-    FibMatrix.new(IDENTITY_VALUES)
-  end
-
-  def *(other_matrix)
-    a1 = @values[0][0] * other_matrix.values[0][0] + @values[1][0] * other_matrix.values[0][1]
-    a2 = @values[0][1] * other_matrix.values[0][0] + @values[1][1] * other_matrix.values[0][1]
-    b1 = @values[0][0] * other_matrix.values[1][0] + @values[1][0] * other_matrix.values[1][1]
-    b2 = @values[0][1] * other_matrix.values[1][0] + @values[1][1] * other_matrix.values[1][1]
-
-    return FibMatrix.new([[a1, a2], [b1, b2]])
+    (m ** n)[0, 1]
   end
 end
